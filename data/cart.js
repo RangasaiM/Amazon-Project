132f03310c84b0830e4f7442fc122612ec286f06
Rangasai <rangasaimangalagiri@gmail.com>
2025-04-18 18:00:27 +0530

    Save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart")) || [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -8,6 +8,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -22,6 +26,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromcart(productId) {
@@ -32,4 +37,5 @@ export function removeFromcart(productId) {
     }
   });
   cart = newcart;
+  saveToStorage();
 }
