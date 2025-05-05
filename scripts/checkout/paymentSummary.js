4ebe1e40ac2707aba912bcfd351dea58bb9d7748
Rangasai <rangasaimangalagiri@gmail.com>
2025-05-05 11:06:47 +0530

    Create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -3,6 +3,8 @@ import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { currencyFormat } from "../utils/money.js";
 
+import { addOrder } from "../../data/orders.js";
+
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
@@ -24,22 +26,22 @@ export function renderPaymentSummary() {
         <div class="payment-summary-row">
         <div>Items (${cartQunatity}):</div>
         <div class="payment-summary-money">$${currencyFormat(
-          productPriceCents
-        )}</div>
+    productPriceCents
+  )}</div>
         </div>
 
         <div class="payment-summary-row">
         <div>Shipping &amp; handling:</div>
         <div class="payment-summary-money">$${currencyFormat(
-          shippingPriceCents
-        )}</div>
+    shippingPriceCents
+  )}</div>
         </div>
 
         <div class="payment-summary-row subtotal-row">
         <div>Total before tax:</div>
         <div class="payment-summary-money">$${currencyFormat(
-          totalBeforeTaxCents
-        )}</div>
+    totalBeforeTaxCents
+  )}</div>
         </div>
 
         <div class="payment-summary-row">
@@ -52,9 +54,34 @@ export function renderPaymentSummary() {
         <div class="payment-summary-money">$${currencyFormat(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
         Place your order
         </button>
   `;
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log('Unexpected error. Try again Later.');
+      }
+
+      window.location.href = 'orders.html';
+    });
 }
+
+
+
+
